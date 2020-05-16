43f4783a3cbc20b9c9bb32abefb1d2bf111be4d4
Enrico Costanzi <enrico.costanzi@gmail.com>
2020-05-16 13:48:53 +0200

    use DynamicPropertySource instead of system properties

--- src/test/java/com/mycompany/myapp/config/DatabaseTestcontainersExtension.java
+++ src/test/java/com/mycompany/myapp/config/DatabaseTestcontainersExtension.java
@@ -15,6 +15,17 @@ public class DatabaseTestcontainersExtension implements BeforeAllCallback {
 
     private static PostgreSQLContainer container;
 
+    @DynamicPropertySource
+    public static void r2dbcProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", () -> container.getJdbcUrl().replace("jdbc", "r2dbc"));
+        registry.add("spring.r2dbc.username", () -> container.getUsername());
+        registry.add("spring.r2dbc.password", () -> container.getPassword());
+
+        registry.add("spring.liquibase.url", () -> container.getJdbcUrl());
+        registry.add("spring.liquibase.user", () -> container.getUsername());
+        registry.add("spring.liquibase.password", () -> container.getPassword());
+    }
+
     @Override
     public void beforeAll(ExtensionContext extensionContext) throws Exception {
 
@@ -23,12 +34,12 @@ public class DatabaseTestcontainersExtension implements BeforeAllCallback {
                 .withDatabaseName("reacttestcnt");
             container.start();
 
-            System.setProperty("spring.r2dbc.url", container.getJdbcUrl().replace("jdbc", "r2dbc"));
-            System.setProperty("spring.r2dbc.username", container.getUsername());
-            System.setProperty("spring.r2dbc.password", container.getPassword());
-            System.setProperty("spring.liquibase.url", container.getJdbcUrl());
-            System.setProperty("spring.liquibase.user", container.getUsername());
-            System.setProperty("spring.liquibase.password", container.getPassword());
+//            System.setProperty("spring.r2dbc.url", container.getJdbcUrl().replace("jdbc", "r2dbc"));
+//            System.setProperty("spring.r2dbc.username", container.getUsername());
+//            System.setProperty("spring.r2dbc.password", container.getPassword());
+//            System.setProperty("spring.liquibase.url", container.getJdbcUrl());
+//            System.setProperty("spring.liquibase.user", container.getUsername());
+//            System.setProperty("spring.liquibase.password", container.getPassword());
 
             started.set(true);
 
